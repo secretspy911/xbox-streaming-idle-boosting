06ae11b15e99b6910aa5bb22554ed2d3e35f5e01
Michaël Rochette <michaelr@icosolutions.com>
2019-05-06 15:24:41 -0400

    WIP: Project creation. Commit before switching to C#.

---
+++ XboxStreamingIdleBoosting/SuperBomberman.vb
@@ -0,0 +1,96 @@
+﻿Imports WindowsInput
+Imports WindowsInput.Native
+
+Public Class SuperBomberman
+    Public Enum Direction
+        Up = 1
+        Down = 2
+        Left = 3
+        Right = 4
+    End Enum
+
+    Private Const MoveOneSquareDelay As Integer = 225
+    Private Const KeyPressDelay As Integer = 500
+    Private Const BombDetonationDelay = 2500
+    Private Const PlaceBombDelay = 1 ' The game needs a delay when getting on a square before being able to place a bomb
+
+    ''' <summary>
+    ''' For this to work, the Xbox App game must be at the main menu.
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub BoostBlocks()
+        PlayMatch()
+    End Sub
+
+    Private Sub Move(direction As Direction, nbSquares As Integer)
+        MainForm.XboxController.Move(direction, nbSquares * MoveOneSquareDelay)
+    End Sub
+
+    Private Sub PlayMatch()
+        ' 1
+        Move(Direction.Right, 1)
+        PlaceBomb() '1,2
+        Move(Direction.Left, 1)
+        Move(Direction.Down, 1)
+        WaitForBomb() '2,1 
+
+        ' 2
+        Move(Direction.Up, 1)
+        Move(Direction.Right, 2)
+        PlaceBomb() ' 1,3
+        Move(Direction.Left, 2)
+        Move(Direction.Down, 1)
+        WaitForBomb() '2,1
+
+        ' 3
+        Move(Direction.Up, 1)
+        Move(Direction.Right, 3)
+        PlaceBomb() '1,4
+        Move(Direction.Left, 1)
+        Move(Direction.Down, 1)
+        WaitForBomb() '2,3
+
+        ' 4
+        PlaceBomb() '2,3
+        Move(Direction.Up, 1)
+        Move(Direction.Right, 2)
+        WaitForBomb() '1,5
+
+        ' 5
+        PlaceBomb() '1,5
+        Move(Direction.Left, 2)
+        Move(Direction.Down, 2)
+        WaitForBomb() '3,3
+
+        ' 6
+        PlaceBomb() '3,3
+        Move(Direction.Up, 2)
+        Move(Direction.Left, 2)
+        Move(Direction.Down, 1)
+        WaitForBomb() '2,1
+
+        ' 7
+        'PlaceBomb() '2,1
+        'Move(Direction.Down, 1)
+        'Move(Direction.Right, 4)
+        'PlaceBomb()
+        'Move(Direction.Left, 2)
+        'Move(Direction.Down, 2)
+        'WaitForBomb()
+
+        '' 8
+        'PlaceBomb()
+        'Move(Direction.Up, 2)
+        'Move(Direction.Left, 1)
+        'WaitForBomb()
+
+    End Sub
+
+    Private Sub PlaceBomb()
+        MainForm.XboxController.PressButton(XboxController.Button.B)
+    End Sub
+
+    Private Sub WaitForBomb()
+        Threading.Thread.Sleep(BombDetonationDelay)
+    End Sub
+End Class
---
+++ XboxStreamingIdleBoosting/XboxController.vb
@@ -0,0 +1,66 @@
+﻿Imports WindowsInput
+Imports WindowsInput.Native
+
+Public Class XboxController
+    Public Enum Direction
+        Up = 1
+        Down = 2
+        Left = 3
+        Right = 4
+    End Enum
+
+    Public Enum Button
+        A = 1
+        B = 2
+        X = 3
+        Y = 4
+    End Enum
+
+    Private Const AButtonKey As VirtualKeyCode = VirtualKeyCode.VK_J
+    Private Const BButtonKey As VirtualKeyCode = VirtualKeyCode.VK_K
+    Private Const XButtonKey As VirtualKeyCode = VirtualKeyCode.VK_U
+    Private Const YButtonKey As VirtualKeyCode = VirtualKeyCode.VK_I
+    Private Const InputDelay As Integer = 50 ' The xbox games cannot take inputs too quickly. Must also make up for network latency.
+    Private Const MoveDelay As Integer = 250 ' Makes up for network latency.
+
+    Public Sub PressButton(button As Button)
+        Dim keyCode As VirtualKeyCode
+        Select Case button
+            Case XboxController.Button.A
+                keyCode = AButtonKey
+            Case XboxController.Button.B
+                keyCode = BButtonKey
+            Case XboxController.Button.X
+                keyCode = XButtonKey
+            Case XboxController.Button.Y
+                keyCode = YButtonKey
+        End Select
+
+        Threading.Thread.Sleep(InputDelay)
+        MainForm.InputSimulator.Keyboard.KeyDown(keyCode)
+        Threading.Thread.Sleep(InputDelay)
+        MainForm.InputSimulator.Keyboard.KeyUp(keyCode)
+    End Sub
+
+    Public Sub Move(direction As Direction, time As Integer)
+        Dim keyCode As WindowsInput.Native.VirtualKeyCode
+        Select Case direction
+            Case SuperBomberman.Direction.Up
+                keyCode = VirtualKeyCode.UP
+            Case SuperBomberman.Direction.Down
+                keyCode = VirtualKeyCode.DOWN
+            Case SuperBomberman.Direction.Left
+                keyCode = VirtualKeyCode.LEFT
+            Case SuperBomberman.Direction.Right
+                keyCode = VirtualKeyCode.RIGHT
+        End Select
+
+        Dim timeSplitCount As Integer = Math.Floor(time / MoveDelay)
+        For i As Integer = 0 To timeSplitCount
+            Threading.Thread.Sleep(InputDelay)
+            MainForm.InputSimulator.Keyboard.KeyDown(keyCode)
+            Threading.Thread.Sleep(MoveDelay)
+            MainForm.InputSimulator.Keyboard.KeyUp(keyCode)
+        Next
+    End Sub
+End Class
